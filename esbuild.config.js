8916d43c110ef899ff9dcdbfaf64df753679ef44
user-ubuntu <userubuntu9@gmail.com>
2025-12-15 01:03:21 +0000

    first commit

---
+++ esbuild.config.js
@@ -0,0 +1,24 @@
+import { build } from "esbuild";
+
+await build({
+  entryPoints: ["src/background/index.ts"],
+  bundle: true,
+  outfile: "dist/background/index.js",
+  platform: "browser",
+  target: "firefox109",
+  format: "iife",
+  globalName: "SessionSwitcher",
+  minify: false,
+  sourcemap: false,
+});
+
+await build({
+  entryPoints: ["src/popup/index.ts"],
+  bundle: true,
+  outfile: "dist/popup/index.js",
+  platform: "browser",
+  target: "firefox109",
+  format: "iife",
+  minify: false,
+  sourcemap: false,
+});
